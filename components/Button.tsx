e73977220173bfc1f06f3efd568f3dfc33c74018
pedro <pedrohhss14@gmail.com>
2023-05-27 11:13:57 -0300

    Feat: create a generic components for use in all project

---
+++ components/Button.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+interface ButtonProps {
+    label: String;
+    secondary?: boolean;
+    fullWidth?: boolean;
+    large?: boolean;
+    onClick: () => void;
+    disabled?: boolean;
+    outiline?: boolean
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    secondary,
+    fullWidth,
+    large,
+    onClick,
+    disabled,
+    outiline,
+}) => {
+    return (
+        <button
+        disabled={disabled}
+        onClick={onClick}
+            className={`
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-full
+        font-semibold
+        hover:opacity-80
+        transition
+        border-2
+        ${fullWidth ? 'w-full' : 'w-fit'}
+        ${secondary ? 'bg-white' : 'bg-sky-500'}
+        ${secondary ? 'text-black' : 'text-white'}
+        ${secondary ? 'border-black' : 'border-sky-500'}
+        ${large ? 'text-xl' : 'text-md'}
+        ${large ? 'px-5' : 'px-4'}
+        ${large ? 'py-3' : 'py-2'}
+        ${outiline ? 'bg-transparent' : ''}
+        ${outiline ? 'border-white' : ''}
+        ${outiline ? 'text-white' : ''}
+        `}>
+            {label}
+        </button>
+    )
+}
+
+export default Button
